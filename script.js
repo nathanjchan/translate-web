e1563cf86c173dea0ca2a9c471bbbc512bd67aea
Nathan Chan <nathanjchan2@gmail.com>
2023-12-08 14:55:35 -0800

    Revert "Try adding translation"
    This reverts commit eb5720cffc182b26cbe17b8715971390307b66fc.

--- script.js
+++ script.js
@@ -6,31 +6,19 @@ var testBtn = document.querySelector('button');
 
 var spokenPhrases = [];
 
-import Translator from "@andreasremdt/simple-translator";
-var translator = new Translator();
-
-function translateText(text) {
-  console.log('Entered translateText()');
-  var translation = {
-    text: text
-  };
-  translator.add('ja', translation);
-  return translator.translateForKey('text', 'ja');
-}
-
 function updateSpokenPhrasesDisplay() {
   var list = document.querySelector('.spoken-phrases');
   list.innerHTML = '';
 
   spokenPhrases.slice().reverse().forEach(function (phrase, index) {
     var listItem = document.createElement('li');
-    listItem.textContent = phrase + ' - ' + translateText(phrase);
+    listItem.textContent = phrase;
     list.appendChild(listItem);
   });
 }
 
 function testSpeech() {
-  console.log('Entered testSpeech()');
+  console.log('testSpeech() activated');
   testBtn.disabled = true;
   testBtn.textContent = 'Translation In Progress';
 
