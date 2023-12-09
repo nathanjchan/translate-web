55cec378b91daba9b51794823ed475cfdbb93d61
Nathan Chan <nathanjchan2@gmail.com>
2023-12-08 18:45:36 -0800

    Oops fix syntax

--- translate.js
+++ translate.js
@@ -1,7 +1,12 @@
 import { pipeline } from 'https://cdn.jsdelivr.net/npm/@xenova/transformers@2.10.1';
 
-export function translate(text) {
-  en_ja_translator = pipeline('translation_en_to_ja');
-  const translated = en_ja_translator(text);
-  return translated[0]['translation_text'];
+export async function translate(text) {
+  try {
+    const en_ja_translator = await pipeline('translation_en_to_ja');
+    const translated = await en_ja_translator(text);
+    return translated[0]['translation_text'];
+  } catch (error) {
+    console.error('Error in translation:', error);
+    return null; // or handle the error as appropriate
+  }
 }
